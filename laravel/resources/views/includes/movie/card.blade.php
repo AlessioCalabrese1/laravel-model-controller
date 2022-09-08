012ef0ba07119be98dc4f174c237e117266aa70f
AlessioCalabrese1 <alessiocalabrese07@gmail.com>
2022-09-08 17:20:14 +0200

    Viewing movies

--- laravel/resources/views/includes/movie/card.blade.php
+++ laravel/resources/views/includes/movie/card.blade.php
@@ -1,10 +1,9 @@
 <div class="card" style="width: 18rem;">
-    @dump($movie)
-    {{-- <ul class="list-group list-group-flush">
-        <li class="list-group-item">{{ $movie->attributes['title'] }}</li>
-        <li class="list-group-item">{{ $movie->attributes['original_title'] }}</li>
-        <li class="list-group-item">{{ $movie->attributes['nationality'] }}</li>
-        <li class="list-group-item">{{ $movie->attributes['date'] }}</li>
-        <li class="list-group-item">{{ $movie->attributes['vote'] }}</li>
-    </ul> --}}
+    <ul class="list-group list-group-flush">
+        <li class="list-group-item">{{ $movie->title }}</li>
+        <li class="list-group-item">{{ $movie->original_title }}</li>
+        <li class="list-group-item">{{ $movie->nationality }}</li>
+        <li class="list-group-item">{{ $movie->date }}</li>
+        <li class="list-group-item">{{ $movie->vote }}</li>
+    </ul>
 </div>
